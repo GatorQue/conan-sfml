3f1a8a0246b5ec823c716ec6d877450946e17647
Ryan Lindeman <ryanlindeman@gmail.com>
2017-02-25 10:43:08 -0700

    travis ci testing

--- .travis/install.sh
+++ .travis/install.sh
@@ -24,15 +24,15 @@ pip install conan_package_tools # It install conan too
 conan user
 
 # Determine if X11 include files are available at the expected locations:
-ls -R /usr/pkg/xorg/include
-ls -R /usr/X11R6/include
-ls -R /usr/X11R7/include
-ls -R /usr/include/X11
-ls -R /usr/openwin/include
-ls -R /usr/openwin/share/include
+[ -d /usr/pkg/xorg/include ] && ls -R /usr/pkg/xorg/include
+[ -d /usr/X11R6/include ] && ls -R /usr/X11R6/include
+[ -d /usr/X11R7/include ] && ls -R /usr/X11R7/include
+[ -d /usr/include/X11 ] && ls -R /usr/include/X11
+[ -d /usr/openwin/include ] && ls -R /usr/openwin/include
+[ -d /usr/openwin/share/include ] && ls -R /usr/openwin/share/include
 
 # Determine if X11 include files are available at the expected locations:
-ls -R /usr/pkg/xorg/lib
-ls -R /usr/X11R6/lib
-ls -R /usr/X11R7/lib
-ls -R /usr/openwin/lib
+[ -d /usr/xorg/lib ] && ls -R /usr/pkg/xorg/lib
+[ -d /usr/X11R6/lib ] && ls -R /usr/X11R6/lib
+[ -d /usr/X11R7/lib ] && ls -R /usr/X11R7/lib
+[ -d /usr/openwin/lib ] && ls -R /usr/openwin/lib
